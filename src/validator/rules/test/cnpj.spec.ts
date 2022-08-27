63e231a9dfbbc8bf010d82f2e444562e0615144b
silvalazaro <lazarofdsilva@gmail.com>
2022-08-27 00:04:20 -0300

    Test created for the CNPJ validator

---
+++ src/validator/rules/test/cnpj.spec.ts
@@ -0,0 +1,15 @@
+
+import { assert, expect, test } from "vitest"
+import { Cnpj } from "@src/validator/rules/cnpj" 
+
+const cnpj = new Cnpj("CNPJ")
+
+test('is valid cnpj', () => {
+  expect(cnpj.isValid('60943725000145')).toBe(true)
+  expect(cnpj.isValid('80223001000134')).toBe(true)
+})
+
+test('is not valid cnpj', () => {
+  expect(cnpj.isValid('')).toBe(false)
+  expect(cnpj.isValid('80223001000124')).toBe(false)
+})
