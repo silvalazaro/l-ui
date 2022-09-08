21b6847d1452016fa9108fcea8c8b51bec596c59
silvalazaro <lazarofdsilva@gmail.com>
2022-09-07 22:56:43 -0300

    fix example of InputText

--- src/components/input/text/examples/InputTextExample.vue
+++ src/components/input/text/examples/InputTextExample.vue
@@ -1,18 +1,21 @@
-<script lang="ts" setup>
-import { InputText } from "@src/components";
-import { ref } from "@vue/reactivity";
-import { ValidatorFactory } from "@src/validator";
-
-const name = ref("1020304040");
-const validator = ValidatorFactory.createValidator("Nome").required();
-</script>
-
 <template>
   <div>
     <input-text
-      v-model="name"
+      v-model="model.name"
       mask="(##).###/##"
+      label="Nome"
     />
+    {{ model.name }}<br>
   </div>
 </template>
 
+<script lang="ts" setup>
+import { InputText } from "@src/components";
+import { reactive, ref } from "@vue/reactivity";
+import { ValidatorFactory } from "@src/validator";
+
+const model = reactive({
+  name: '82'
+});
+
+</script>
